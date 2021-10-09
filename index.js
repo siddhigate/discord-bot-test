16084b20999cc234d8a02a05cdc58a767417b607
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-10-09 23:45:36 +0530

    feat: message reply

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', async msg => {
+
+  switch (msg.content) {
+    case "hi":
+      msg.reply("bye");
+      break;
+
+    case "bye":
+      msg.reply("leave");
+      break;
+   }
+})
 
 client.login(process.env.TOKEN); 
